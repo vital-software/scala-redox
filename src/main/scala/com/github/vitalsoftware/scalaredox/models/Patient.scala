d83e988c0e29c347d54e6d6fbb75bfddfd1d3c14
Mikhail Kokho <m.kokho@gmail.com>
2017-05-29 12:50:00 +0800

    use LocalDate type for DOB in Demographics case class

--- src/main/scala/com/github/vitalsoftware/scalaredox/models/Patient.scala
+++ src/main/scala/com/github/vitalsoftware/scalaredox/models/Patient.scala
@@ -1,6 +1,6 @@
 package com.github.vitalsoftware.scalaredox.models
 
-import org.joda.time.DateTime
+import org.joda.time.LocalDate
 import com.github.vitalsoftware.util.JsonImplicits.jodaDateFormat
 import com.github.vitalsoftware.macros._
 import play.api.libs.json.Reads.DefaultJodaDateReads
@@ -43,7 +43,7 @@ object Gender extends Enumeration {
 @jsonDefaults case class Demographics(
   FirstName: String,
   LastName: String,
-  DOB: DateTime,
+  DOB: LocalDate,
   SSN: Option[String] = None,
   Sex: Gender.Value,
   Address: Option[Address] = None,
